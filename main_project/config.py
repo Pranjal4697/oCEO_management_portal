1e81997a551d5cf0f46fce64c71e6c24654e4b51
pranjal4697 <pranjal4697@gmail.com>
2024-10-30 23:19:55 +0530

    updated config

--- main_project/config.py
+++ main_project/config.py
@@ -3,4 +3,3 @@ class Config:
     SECRET_KEY = 'mySecretKey'
     SQLALCHEMY_DATABASE_URI =  os.environ.get('DATABASE_URL')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SERVER_NAME = '127.0.0.1:5000'
